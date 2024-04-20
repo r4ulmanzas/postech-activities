cc7cf407b6aa9aea9a29115fa3255aac75aa431f
r4ulmanzas <raulmanzas@gmail.com>
2024-04-20 15:22:58 -0300

    allows cors requests for the challenge specific controller

--- docker/challenge/backend/challenge-api/src/main/java/com/challenge/fiap/challengeapi/GetBookController.java
+++ docker/challenge/backend/challenge-api/src/main/java/com/challenge/fiap/challengeapi/GetBookController.java
@@ -1,12 +1,14 @@
 package com.challenge.fiap.challengeapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class GetBookController {
 
     @Autowired
