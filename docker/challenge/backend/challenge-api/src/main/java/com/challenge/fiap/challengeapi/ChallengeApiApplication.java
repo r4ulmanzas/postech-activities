85f6ff48bce6bc60cde1477eb28f2c8c51d610ff
r4ulmanzas <raulmanzas@gmail.com>
2024-04-20 10:19:12 -0300

    adds back structure for the challenge

---
+++ docker/challenge/backend/challenge-api/src/main/java/com/challenge/fiap/challengeapi/ChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.fiap.challengeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApiApplication.class, args);
+	}
+
+}
---
+++ docker/challenge/backend/challenge-api/src/test/java/com/challenge/fiap/challengeapi/ChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.fiap.challengeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
